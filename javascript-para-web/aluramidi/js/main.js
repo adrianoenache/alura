80294edb7e70939cd7a8d63c6454378d5e74aabb
Adriano Enache <adrianoenache@hotmail.com>
2022-11-01 11:44:55 -0300

    Adicionados comentários ao JS.

--- javascript-para-web/aluramidi/js/main.js
+++ javascript-para-web/aluramidi/js/main.js
@@ -1,3 +1,21 @@
+/*
+
+  Reaproveitar código é ganhar tempo. Para escrever código
+  que possa ser facilmente reaproveitado ele deve ser pequeno,
+  especialista, fazer uma única coisa, ser uma peça de lego
+  capaz de se conectar com outras obtendo resultados diferentes.
+
+*/
+
+/*
+
+  As funções $getMySelector e $getMySelectors são seletores de
+  elementos e fazem um teste, verificando se o/s elemento/s existem
+  na página, retornando um aviso no console em caso negativo ou
+  retornando o elemento ou a lista de elementos para a quem
+  chamou estas funções.
+
+*/
 const $getMySelector = ($target) => {
 
   if(document.querySelector($target)) {
@@ -26,6 +44,19 @@ const $getMySelectors = ($targets) => {
 
 }
 
+/*
+
+  As funções addCssClass e removeCssClass recebem como primeiro parâmetro
+  um elemento alvo ou a string de um seletor CSS e como segundo parâmetro
+  uma classe de CSS ex.: ".active". Estas funções fazem duas verificações
+  a primeira através de uma outra função checkIfMySelectorIsString que verifica
+  se o valor do primeiro parâmetro é uma string na forma de um seletor CSS
+  se verdadeiro a função retornara o elemento e se falso significa que
+  o target já é um elemento selecionado e o retorna. A segunda verificação
+  testa de o elemento alvo tem ou não a classe passada no segundo parâmetro
+  para então adicionar ou remover a classe CSS.
+
+*/
 const checkIfMySelectorIsString = ($target) => {
 
   if(typeof $target === 'string') {
@@ -64,6 +95,15 @@ const removeCssClass = ($target, cssClass) => {
 
 }
 
+/*
+
+  A função playUsingKeyboard recebe como parâmetro o valor de código
+  de uma tecla pressionada. Verificando através dos casos cadastrados,
+  onde para cada caso, a função retorna um valor associado a tecla ou
+  retorna falso, caso a tecla pressionada não corresponda a nenhum dos
+  casos.
+
+*/
 const playUsingKeyboard = (playWhat) => {
 
   switch (playWhat) {
@@ -102,6 +142,16 @@ const playUsingKeyboard = (playWhat) => {
 
 }
 
+/*
+
+  A função playSound recebe como parâmetro uma string para
+  compor um seletor CSS, que é usado pela função $getMySelector
+  para obter o valor de src do elemento para ser executada
+  usando a HTML DOM API HTMLAudioElement. É possível reproduzir
+  um som usando $getMySelector(_target).play(), especificamente
+  neste caso irá causar um bug pelo comportamento que é esperado.
+
+*/
 const playSound = ($target) => {
 
   let _target = `#${$target}`;
@@ -113,6 +163,13 @@ const playSound = ($target) => {
 
 }
 
+/*
+
+  A função aluramidi é uma função de um componente que utiliza as funções
+  anteriores para compor suas ações. Nela são atribuídos eventos de click
+  a botões e eventos de teclado para executar suas ações.
+
+*/
 const aluramidi = () => {
 
   const elementSelector = '.aluramidi';
@@ -177,6 +234,14 @@ const aluramidi = () => {
 
 }
 
+/*
+
+  Este evento foi adicionado ao window e faz com que as funções
+  em seu interior só sejam executadas após o load da página, evitando
+  assim que o javascript faça interações com a página antes dela
+  estar pronta para isso.
+
+*/
 window.addEventListener('load', () => {
 
   aluramidi();
