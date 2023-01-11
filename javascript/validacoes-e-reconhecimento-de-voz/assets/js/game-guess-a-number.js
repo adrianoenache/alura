5461ece03faf3d17ee01598d156030a214873f7e
Adriano Enache <adrianoenache@hotmail.com>
2023-01-11 19:30:29 -0300

    Aula 4 - Parcial

--- javascript/validacoes-e-reconhecimento-de-voz/assets/js/game-guess-a-number.js
+++ javascript/validacoes-e-reconhecimento-de-voz/assets/js/game-guess-a-number.js
@@ -9,6 +9,7 @@ import { $getMySelector } from './common-functions.js';
 let guessThisNumber = '';
 const limitNumber = 1000;
 const baseNumber = 1;
+const elementResult = $getMySelector('#chute');
 
 export function startGameGessANumber() {
 
@@ -21,6 +22,8 @@ export function startGameGessANumber() {
   elementMinValue.innerHTML = baseNumber;
   elementMaxValue.innerHTML = limitNumber;
 
+  startWebSpeech();
+
 }
 
 function raffleANumber(setLimitNumber) {
@@ -28,5 +31,91 @@ function raffleANumber(setLimitNumber) {
   const raffle = Math.random() * setLimitNumber + 1;
 
   return parseInt(raffle);
+
+}
+
+/*
+  A função startWebSpeech inicializa as configurações do web speech API
+*/
+function startWebSpeech() {
+
+  window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+  const newSpeechRecognition = new SpeechRecognition();
+
+  newSpeechRecognition.lang = 'pt-BR';
+  newSpeechRecognition.start();
+
+  newSpeechRecognition.addEventListener('result', onSpeack);
+
+}
+
+function onSpeack(event) {
+
+  const myGuess = event.results[0][0].transcript;
+  elementResult.innerHTML = `<div>Você disse:</div><span class="box">${myGuess}</span>`;
+
+  verifyMyGuess(myGuess);
+
+}
+
+function verifyMyGuess(myGuess) {
+
+  let number = +myGuess;
+
+  if(myGuess == 'Zero.') {
+
+    elementResult.innerHTML += '<div>O valor não pode ser zero.</div>';
+
+  }
+
+  if(myGuess == 'Um.') {
+
+    number = 1;
+
+  }
+
+  if(checkIfIsNumber(number) && myGuess !== 'Zero.') {
+
+    elementResult.innerHTML += '<div>Valor inválido, fale um número.</div>';
+
+  }
+
+  if(checkIfNumberIsInRange(number)) {
+
+    elementResult.innerHTML += `<div>O valor inválido, escolha um número entre ${baseNumber} e ${limitNumber}</div>`;
+
+  }
+
+  if(number < guessThisNumber) {
+
+    elementResult.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>`;
+
+  }
+
+  if(number > guessThisNumber) {
+
+    elementResult.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>`;
+
+  }
+
+  if(number == guessThisNumber) {
+
+    elementResult.innerHTML += `<div>Você acertou o número sorteado ${guessThisNumber}.</div>`;
+
+  }
+
+  console.log('### myGuess ', myGuess);
+
+}
+
+function checkIfIsNumber(value) {
+
+  return Number.isNaN(value);
+
+}
+
+function checkIfNumberIsInRange(value) {
+
+  return value > limitNumber || value < baseNumber;
 
 }
