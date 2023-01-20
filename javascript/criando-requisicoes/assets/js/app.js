cfe43b9eb748439dbd338c2759694bf19be8f4eb
Adriano Enache <adrianoenache@hotmail.com>
2023-01-20 18:53:02 -0300

    Setup arquivos do curso.

---
+++ javascript/criando-requisicoes/assets/js/app.js
@@ -0,0 +1,27 @@
+/*
+
+  Para os imports funcionem, é necessário que no HTML onde é importado
+  o arquivo "app.js", o parâmetro "type" tenha o valor "module".
+
+*/
+// import { $getMySelector } from './common-functions.js';
+
+const initCode = () => {
+
+  console.log('Setup');
+
+}
+
+/*
+
+  Este evento foi adicionado ao window e faz com que as funções
+  em seu interior só sejam executadas após o load da página, evitando
+  assim que o javascript faça interações com a página antes dela
+  estar pronta para isso.
+
+*/
+window.addEventListener('load', () => {
+
+  initCode();
+
+});
