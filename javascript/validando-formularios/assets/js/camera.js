9ef27de38f8ba40061c0210eb263abd1abd6bc1d
Adriano Enache <adrianoenache@hotmail.com>
2023-06-27 19:39:21 -0300

    Aula 5

--- javascript/validando-formularios/assets/js/camera.js
+++ javascript/validando-formularios/assets/js/camera.js
@@ -1,10 +1,16 @@
-import { $getMySelector, onTargetEventDoAction, removeCssClass, addCssClass } from './common-functions.js';
+import { $getMySelector, onTargetEventDoAction, removeCssClass, addCssClass, getLocalStorageData, setLocalStorageData } from './common-functions.js';
 
 export function takePicture () {
 
   const startCam = $getMySelector('[data-video-botao]');
   const cam = $getMySelector('[data-camera]');
   const video = $getMySelector('[data-video]');
+  const buttonGetPhoto = $getMySelector('[data-tirar-foto]');
+  const videoCanvas = $getMySelector('[data-video-canvas]');
+  const messageField = $getMySelector('[data-mensagem]');
+  const buttonSendPhoto = $getMySelector('[data-enviar]');
+
+  let imagemURL = '';
 
   if(!startCam && !cam && !video) return;
 
@@ -19,6 +25,27 @@ export function takePicture () {
     removeCssClass(cam, 'hide');
 
     video.srcObject = startVideoStream;
+
+  });
+
+  onTargetEventDoAction(buttonGetPhoto, 'click', () => {
+
+    videoCanvas.getContext('2d').drawImage(video, 0, 0, videoCanvas.width, videoCanvas.height);
+
+    imagemURL = videoCanvas.toDataURL('image/jpg');
+
+    addCssClass(cam, 'hide');
+    removeCssClass(messageField, 'hide');
+
+  });
+
+  onTargetEventDoAction(buttonSendPhoto, 'click', () => {
+
+    const updateLocalStorageData = getLocalStorageData('cadastro');
+    updateLocalStorageData.imagem = imagemURL;
+    setLocalStorageData('cadastro', updateLocalStorageData);
+
+    window.location.href='./abrir-conta-form-3.html';
 
   });
 
